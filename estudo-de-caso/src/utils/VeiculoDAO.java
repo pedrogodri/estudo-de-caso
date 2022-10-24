ac65893dc887d3a9a4b1f4e65de1d0ebfea2b485
Pedro Henrique Godri <pedrogodri@gmail.com>
2022-10-24 20:36:35 -0300

    Commit - Removação de ArrayList

--- estudo-de-caso/src/utils/VeiculoDAO.java
+++ estudo-de-caso/src/utils/VeiculoDAO.java
@@ -11,9 +11,9 @@ import model.Onibus;
 
 public class VeiculoDAO {
 	ArrayList<Veiculo> listaVeiculo = new ArrayList();
-	ArrayList<Pesados> listaPesados = new ArrayList();
-	ArrayList<Carro> listaCarro = new ArrayList();
-	ArrayList<Moto> listaMoto = new ArrayList();
-	ArrayList<Caminhao> listaCaminhao = new ArrayList();
-	ArrayList<Onibus> listaOnibus = new ArrayList();
+	//ArrayList<Pesados> listaPesados = new ArrayList();
+	//ArrayList<Carro> listaCarro = new ArrayList();
+	//ArrayList<Moto> listaMoto = new ArrayList();
+	//ArrayList<Caminhao> listaCaminhao = new ArrayList();
+	//ArrayList<Onibus> listaOnibus = new ArrayList();
 }
