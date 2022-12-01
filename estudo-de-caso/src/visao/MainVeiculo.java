6a7116ebb347ab95d00ce034534e3f4ec61112bd
Bárbara <barbara.n2005@aluno.ifsc.edu.br>
2022-12-01 14:06:59 -0300

    Commit - Case 3 alterado

--- estudo-de-caso/src/visao/MainVeiculo.java
+++ estudo-de-caso/src/visao/MainVeiculo.java
@@ -136,18 +136,49 @@ public class MainVeiculo {
 			case 3: {
 				limparConsole();
 				Veiculo veiculo = new Veiculo();
-				System.out.println("╔════════════════════════╗\n");
-				System.out.println("║  ID    MARCA    MODELO ║\n");
+				Integer opcao;
+				Boolean encontrou = false;
+				Integer id;
 				
-				for (Veiculo v : listaVeiculos) {
-					System.out.println("║ " + v.getId() + v.getModelo() + " ║\n");
+				while (true)
+				{
+					System.out.println("╔════════════════════════╗\n");
+					System.out.println("║  ID    MARCA    MODELO ║\n");
+					
+					for (Veiculo v : listaVeiculos) {
+						System.out.println("║ " + v.getId() + v.getModelo() + " ║\n");
+					}
+					System.out.println("╚══════════════╝\n");
+					
+					System.out.println("Informe o id do veiculo para excluir: ");
+					id = Integer.valueOf(ler.nextLine());
+					
+					if(id.equals(0))
+					{
+						System.out.println("Você saiu");
+						break;
+					}
+					
+					for (Veiculo v : listaVeiculos) {
+						if(id.equals(v.getId()))
+						{
+							encontrou = true;
+							listaVeiculos.remove(v);
+							System.out.println("Operação feita com sucesso");
+							break;
+						}
+					}
+					
+					if(encontrou == false) 
+					{
+						System.out.println("Codigo informado nao existe");
+					}
+					
+					
 				}
-				
-				
-				System.out.println("╚══════════════╝\n");
-				//Barbara
 			}
 			case 4: {
+				limparConsole();
 				limparConsole();
 				Veiculo veiculo = new Veiculo();
 				System.out.println("╔════════════════════════╗\n");
